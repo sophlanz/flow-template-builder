f4228c610205742b4238ec22f33af054c00371c0
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-05-19 16:13:04 -0400

    new lazy load message for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { lazy, Suspense } from 'react';
+import { Typography } from '@mui/material';
 const Home = lazy(() => import('./pages/Home'));
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading..</div>}>
+      <Suspense
+        fallback={
+          <Typography
+            variant="h1"
+            sx={{
+              position: 'absolute',
+              left: '50%',
+              top: '50%',
+              transform: 'translate(-50%,-50%)',
+              color: 'blue',
+            }}
+          >
+            Template Builder
+          </Typography>
+        }
+      >
         <Home />
       </Suspense>
     </div>
